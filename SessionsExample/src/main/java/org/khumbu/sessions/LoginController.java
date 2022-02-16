686e6451b22765f774b73019b0e338cd69ad81e0
Khumbulani Ndlovu <khumbulanindlovu@mail.weber.edu>
2022-02-15 18:39:43 -0700

    session example

---
+++ SessionsExample/src/main/java/org/khumbu/sessions/LoginController.java
@@ -0,0 +1,54 @@
+package org.khumbu.sessions;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginController
+ */
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username= request.getParameter("username");
+		String password= request.getParameter("password");
+		if(username.equals("khumbu") && password.equals("mbeki"))
+		{
+			//invalidate session if there is one
+			request.getSession().invalidate();
+			HttpSession newSession= request.getSession(true);
+			newSession.setMaxInactiveInterval(300);
+			response.sendRedirect("home.jsp");
+		}
+		else
+		{
+			response.sendRedirect("index.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ SessionsExample/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Login</title>
+</head>
+<body>
+<form action="<%= request.getContextPath() %>/LoginController" method="post">
+<label for="username">Username :</label> <input type="text" name="username" ><br/><br/>
+<label for="password">Password :</label> <input type="password" name="password" >
+<input type="submit" value="Login">
+</form>
+</body>
+</html>
